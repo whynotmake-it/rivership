bb7041879664e0dd3e4f32aab65bdb4240c3552b
Tim Lehmann <hi@timcreated.it>
2024-08-12 20:11:16 +0200

    feat: `usePageIndex` hook to only trigger rebuilds when the page changes, not any inbetween values

--- packages/rivership/lib/src/hooks/use_page.dart
+++ packages/rivership/lib/src/hooks/use_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:rivership/rivership.dart';
+import 'package:rivership/src/hooks/use_keyed_state.dart';
+import 'package:rivership/src/hooks/use_on_listenable_change.dart';
 
 /// Returns the current page of a [PageController].
 ///
@@ -7,7 +10,29 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 /// [PageController] has no dimensions.
 double usePage(PageController controller) {
   final pc = useListenable(controller);
-  return pc.positions.isNotEmpty && pc.position.haveDimensions
-      ? pc.page ?? pc.initialPage.toDouble()
-      : pc.initialPage.toDouble();
+  return pc.tryPage;
+}
+
+/// Returns the current whole page of a [PageController] as an integer.
+///
+/// Only triggers a rebuild when that changes, not for the inbetween values
+/// during transitions.
+int usePageIndex(PageController controller) {
+  final initialPage = useKeyedState(
+    controller.tryPage.round(),
+    keys: [controller],
+  );
+
+  useOnListenableChange(controller, () {
+    if (initialPage.value != controller.tryPage.round()) {
+      initialPage.value = controller.tryPage.round();
+    }
+  });
+  return initialPage.value;
+}
+
+extension on PageController {
+  double get tryPage => positions.isNotEmpty && position.haveDimensions
+      ? page ?? initialPage.toDouble()
+      : initialPage.toDouble();
 }
--- packages/rivership/test/src/hooks/use_page_test.dart
+++ packages/rivership/test/src/hooks/use_page_test.dart
@@ -50,6 +50,7 @@ void main() {
 
     testWidgets('returns initial page if it is a different page',
         (tester) async {
+      controller.dispose();
       controller = PageController(initialPage: 1);
       await tester.pumpWidget(build(showPageView: false));
       expect(find.byKey(const ValueKey(1.0)), findsOneWidget);
@@ -58,6 +59,7 @@ void main() {
     testWidgets('updates when mounted', (tester) async {
       // Test page counts from 1 to 1000
       for (final pages in List<int>.generate(1000, (index) => index + 1)) {
+        controller.dispose();
         // Set inital page too high
         controller = PageController(initialPage: pages);
         // Start by not showing the page view
@@ -81,6 +83,86 @@ void main() {
       controller.jumpToPage(1);
       await tester.pump();
       expect(find.byKey(const ValueKey(1.0)), findsOneWidget);
+    });
+  });
+
+  group('usePageIndex', () {
+    late PageController controller;
+    setUp(() {
+      controller = PageController();
+      addTearDown(() => controller.dispose());
+    });
+
+    Widget build({
+      bool showPageView = true,
+      int pages = 10,
+    }) {
+      return MaterialApp(
+        home: HookBuilder(
+          builder: (context) {
+            final page = usePageIndex(controller);
+            return Stack(
+              children: [
+                if (showPageView)
+                  PageView.builder(
+                    controller: controller,
+                    itemCount: pages,
+                    itemBuilder: (context, index) => Container(),
+                  ),
+                SizedBox.expand(
+                  key: ValueKey(page),
+                ),
+              ],
+            );
+          },
+        ),
+      );
+    }
+
+    test('is exported by package', () async {
+      expect(usePageIndex, isA<Function>());
+    });
+
+    testWidgets('returns initial page if no clients', (tester) async {
+      await tester.pumpWidget(build(showPageView: false));
+      expect(find.byKey(const ValueKey(0)), findsOneWidget);
+    });
+
+    testWidgets('returns initial page if it is a different page',
+        (tester) async {
+      controller.dispose();
+      controller = PageController(initialPage: 1);
+      await tester.pumpWidget(build(showPageView: false));
+      expect(find.byKey(const ValueKey(1)), findsOneWidget);
+    });
+
+    testWidgets('updates when mounted', (tester) async {
+      // Test page counts from 1 to 1000
+      for (final pages in List<int>.generate(1000, (index) => index + 1)) {
+        controller.dispose();
+        // Set inital page too high
+        controller = PageController(initialPage: pages);
+        // Start by not showing the page view
+        await tester.pumpWidget(
+          build(showPageView: false),
+        );
+        expect(find.byKey(ValueKey(pages)), findsOneWidget);
+        // Now show the page view with the correct number of pages
+        await tester.pumpWidget(build(pages: pages));
+        await tester.pumpAndSettle();
+        // The controller should reduce the page by one to match the page view
+        expect(controller.page, pages - 1);
+
+        // The page view should show the correct page
+        expect(find.byKey(ValueKey(pages - 1)), findsOneWidget);
+      }
+    });
+
+    testWidgets('returns the current page', (tester) async {
+      await tester.pumpWidget(build());
+      controller.jumpToPage(1);
+      await tester.pump();
+      expect(find.byKey(const ValueKey(1)), findsOneWidget);
     });
   });
 }
