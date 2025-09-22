437b41ee1480ac94d9e79109816f8dfc79bd186e
Tim Lehmann <hi@timcreated.it>
2025-09-22 23:30:55 +0200

    linter love

--- packages/motor/test/motion_sequence_test.dart
+++ packages/motor/test/motion_sequence_test.dart
@@ -1,4 +1,5 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors,
+// ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:flutter/physics.dart';
 import 'package:flutter_test/flutter_test.dart';
