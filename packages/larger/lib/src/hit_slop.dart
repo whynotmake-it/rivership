5b0aecbd9a65a93b4dec899e20042568f6ff8041
Tim Lehmann <hi@timcreated.it>
2025-12-19 09:27:55 +0100

    doc

--- packages/larger/lib/src/hit_slop.dart
+++ packages/larger/lib/src/hit_slop.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
-class HitSlop extends SingleChildRenderObjectWidget {}
+/// Where to redirect taps that land in the hit slop area.
+enum TapRedirectionPolicy {
+  /// Redirect taps that land in the hit slop area to the center of the child.
+  center,
 
-class RenderHitSlop extends RenderTapRegion {}
+  /// Redirect taps that land in the hit slop area to the nearest edge of the
+  /// child.
+  nearestEdge,
+}
+
+/// A widget that adds hit slop to its child.
+///
+/// Internally, this uses a [TapRegion] to montior any taps outside this widget.
+class HitSlop extends SingleChildRenderObjectWidget {
+  const HitSlop({
+    required this.hitSlop,
+    required this.child,
+    super.key,
+  });
+
+  final EdgeInsetsGeometry hitSlop;
+
+  final Widget child;
+}
